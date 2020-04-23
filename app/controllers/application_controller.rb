a70f482138d09dfebd6c44d0b5fa02009c3d310d
mackmcquen <mackmcquen@comcast.net>
2020-04-23 13:14:01 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,52 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+  
+  # NEW
+  get '/articles/new' do
+    erb :new
+  end
+
+  # CREATE
+  post '/articles' do
+    @article = Article.create(title: params["title"], content: params["content"])
+
+    redirect "/articles/#{@article.id}"
+  end
+
+  # INDEX
+  get '/articles' do
+    @articles = Article.all
+    
+    erb :index
+  end
+
+  # SHOW
+  get "/articles/:id" do
+    @article = Article.find(params[:id])
+    
+    erb :show
+  end
+
+  # EDIT
+  get "/articles/:id/edit" do
+    @article = Article.find(params[:id])
+
+    erb :edit
+  end
+
+  # UPDATE
+  patch "/articles/:id" do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+
+    redirect "/articles/#{@article.id}"
+  end
+  
+  # DELETE
+  delete "/articles/:id" do
+    Article.destroy(params[:id])
+
+    redirect '/articles'
+  end
 end
